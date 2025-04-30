faf578c579fa3e36b4ac1a6600ac30345640ad2b
Shubhanshu Trivedi <itsshubhanshu03@gmail.com>
2025-04-30 12:45:34 +0530

    Set up MonogDB

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  connectDB();
 });
